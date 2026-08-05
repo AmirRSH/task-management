cd5e6417898d70fabd4eb2b966375ec37826efe5
AmirRSh <roshaniamirho3ein@gmail.com>
2026-08-05 15:51:33 +0330

    feat: setup Pinia task store

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,3 +1,13 @@
+<script setup>
+import { useTaskStore } from '@/stores/task'
+
+const taskStore = useTaskStore()
+</script>
+
 <template>
   <h1>Home Page</h1>
+
+  <p>
+    Tasks count: {{ taskStore.tasks.length }}
+  </p>
 </template>
---
+++ src/stores/task.js
@@ -0,0 +1,11 @@
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+
+export const useTaskStore = defineStore('task', () => {
+
+  const tasks = ref([])
+
+  return {
+    tasks,
+  }
+})
