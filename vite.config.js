4ff294bd25402aae2f2ab8edf3e7a158a656531f
AmirRSh <roshaniamirho3ein@gmail.com>
2026-08-05 16:16:30 +0330

    feat: setup Tailwind and Vitest environment

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,21 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'node:path'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'node:path'
 
 export default defineConfig({
   plugins: [
     vue(),
     tailwindcss(),
   ],
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
+  },
+
+  test: {
+    environment: 'jsdom',
   },
 })
