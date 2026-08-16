cefc3fb1e914e90b8f6387dff5ebeece56ffab18
AmirRSh <roshaniamirho3ein@gmail.com>
2026-08-16 16:47:49 +0330

    feat : test for successful logs in

--- src/tests/auth.spec.js
+++ src/tests/auth.spec.js
@@ -20,6 +20,39 @@ describe('Auth Store', () => {
         expect(authStore.user).toBeNull()
         expect(localStorage.getItem('user')).toBeNull()         
     })
+    it('logs in the user successfully', async ()=>{
+        const mockUser = {
+            id: '1',
+            name: 'AmirRSH',
+            email: 'amir@example.test',
+            password: '123456',
+        }
+        global.fetch = vi.fn().mockResolvedValue({
+            ok: true,
+            json: async () => [mockUser],
+        })
+        const authStore = useAuthStore()
+        await authStore.login(
+            'amir@example.test',
+            '123456'
+        )
+        expect(authStore.user).toEqual({
+            id: '1',
+            name: 'AmirRSH',
+            email: 'amir@example.test',
+        })
+        expect(localStorage.getItem('user')).toBe(
+            JSON.stringify({
+                id: '1',
+                name: 'AmirRSH',
+                email: 'amir@example.test',
+            })
+        )    
+        expect(global.fetch).toHaveBeenCalledTimes(1)
+        expect(global.fetch).toHaveBeenCalledWith(
+            'http://localhost:3000/users'
+        )
+    })
     
 
  })
