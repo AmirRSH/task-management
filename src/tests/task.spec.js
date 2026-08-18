70438449996ee97834bb143d8bffb7f4f24fcec5
AmirRSh <roshaniamirho3ein@gmail.com>
2026-08-18 18:07:25 +0330

    feat : test of fetches tasks successfully

---
+++ src/tests/task.spec.js
@@ -0,0 +1,34 @@
+import { describe , it , expect , beforeEach , vi } from "vitest";
+import { createPinia, setActivePinia } from 'pinia'
+import { useTaskStore } from '@/stores/task'
+
+
+describe('Task Store', ()=>{
+    beforeEach(() => {
+        setActivePinia(createPinia())
+    })
+    it('fetches tasks successfully',async ()=>{
+        const mockTasks = [
+            {
+                id: '1',
+                title: 'Learn Vue',
+                description: 'Practice Vue',
+                status: 'open',
+            },
+            {
+                id: '2',
+                title: 'Read docs',
+                description: 'Read Vue docs',
+                status: 'done',
+            },
+        ]
+        global.fetch = vi.fn().mockResolvedValue({
+            ok: true,
+            json: async () => mockTasks,
+        })
+        const taskStore = useTaskStore()
+
+        await taskStore.fetchTasks()
+        expect(taskStore.tasks).toEqual(mockTasks)
+    })
+})
