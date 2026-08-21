fb4c11f0e250c776ff4d03c6981446d3e96b0e3d
AmirRSh <roshaniamirho3ein@gmail.com>
2026-08-21 15:27:52 +0330

    test : shows error when signup fields are empty

--- src/tests/login.spec.js
+++ src/tests/login.spec.js
@@ -118,7 +118,7 @@ describe('LoginPage', () => {
         const wrapper = mount(LoginPage, {
             global: {
                 plugins: [router, pinia],
-            
+
                 stubs: {
                     VContainer: {
                         template: '<div><slot /></div>',
@@ -152,18 +152,71 @@ describe('LoginPage', () => {
                 },
             },
         })
-    
+
         const signupButton = wrapper
             .findAll('button')
             .find(button => button.text() === 'Sign Up')
-    
+
         await signupButton.trigger('click')
-    
+
         expect(wrapper.text()).toContain('Create your account')
-    
+
         const inputs = wrapper.findAll('input')
-    
+
         expect(inputs).toHaveLength(4)
     })
+    it('shows error when signup fields are empty', async () => {
+    const wrapper = mount(LoginPage, {
+        global: {
+            plugins: [router, pinia],
+
+            stubs: {
+                VContainer: {
+                    template: '<div><slot /></div>',
+                },
+                VCard: {
+                    template: '<div><slot /></div>',
+                },
+                VCardTitle: {
+                    template: '<div><slot /></div>',
+                },
+                VCardSubtitle: {
+                    template: '<div><slot /></div>',
+                },
+                VCardText: {
+                    template: '<div><slot /></div>',
+                },
+                VAlert: {
+                    template: '<div><slot /></div>',
+                },
+                VTextField: {
+                    template: '<input />',
+                },
+                VBtn: {
+                    emits: ['click'],
+                    template: `
+                        <button @click="$emit('click')">
+                            <slot />
+                        </button>
+                    `,
+                },
+            },
+        },
+    })
+
+    const signupButton = wrapper
+        .findAll('button')
+        .find(button => button.text() === 'Sign Up')
+
+    await signupButton.trigger('click')
+
+    const createAccountButton = wrapper
+        .findAll('button')
+        .find(button => button.text() === 'Create Account')
+
+    await createAccountButton.trigger('click')
+
+    expect(wrapper.text()).toContain('All fields are required')
+})
     
 })
